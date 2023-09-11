ffaff1bc8084b027234c1aa7cf07f3fd625d0628
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-09-11 08:59:32 +0200

    added nav bar mark up

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,19 +3,52 @@ export default {
   data() {
     return {
       message: "La mia prima app con Vite!",
+      webPages: [
+        "characters",
+        "comics",
+        "movies",
+        "tv",
+        "games",
+        "collectibles",
+        "videos",
+        "fans",
+        "news",
+        "shop",
+      ],
     };
   },
 };
 </script>
 
 <template>
-  <header>
-    <h1>{{ message }}</h1>
-  </header>
+  <div class="container">
+    <nav class="nav-bar d-flex">
+      <div class="col-4">
+        <img src="../../public/img/dc-logo.png" alt="logo.png" />
+      </div>
+      <div class="d-flex col">
+        <span v-for="item in webPages" href="#"> {{ item }}</span>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-h1 {
-  font-size: 3rem;
+.nav-bar {
+  justify-content: space-between;
+  align-items: center;
+
+  .col-4 {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    img {
+      width: 5rem;
+      aspect-ratio: 1;
+    }
+  }
+  div.d-flex {
+    justify-content: space-around;
+  }
 }
 </style>
