21ccc5bfc2ca39930b70fa28456b48a550b5270c
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-09-11 08:23:25 +0200

    first commit

---
+++ src/components/AppMain.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      message: "La mia prima app con Vite!",
+    };
+  },
+};
+</script>
+
+<template>
+  <header>
+    <h1>{{ message }}</h1>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+  font-size: 3rem;
+}
+</style>
