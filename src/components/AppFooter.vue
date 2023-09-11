a16964b7b002e96db9da3bd9a83e72956f74dd52
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-09-11 16:22:44 +0200

    added footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -15,36 +15,41 @@ export default {
 <template>
   <div class="top-footer">
     <div class="container d-flex">
-      <ul>
-        <h1>DC COMICS</h1>
-        <li v-for="(item, index) in footerListComics" :key="index">
-          {{ item }}
-        </li>
-      </ul>
-      <ul>
-        <h1>SHOP</h1>
-        <li v-for="(item, index) in footerListShop" :key="index">
-          {{ item }}
-        </li>
-      </ul>
-      <ul>
-        <h1>DC</h1>
-        <li v-for="(item, index) in footerListDC" :key="index">
-          {{ item }}
-        </li>
-      </ul>
-      <ul>
-        <h1>SITES</h1>
-        <li v-for="(item, index) in footerListSites" :key="index">
-          {{ item }}
-        </li>
-      </ul>
+      <div class="links d-flex">
+        <ul>
+          <h1>DC COMICS</h1>
+          <li v-for="(item, index) in footerListComics" :key="index">
+            {{ item }}
+          </li>
+        </ul>
+        <ul>
+          <h1>SHOP</h1>
+          <li v-for="(item, index) in footerListShop" :key="index">
+            {{ item }}
+          </li>
+        </ul>
+        <ul>
+          <h1>DC</h1>
+          <li v-for="(item, index) in footerListDC" :key="index">
+            {{ item }}
+          </li>
+        </ul>
+        <ul>
+          <h1>SITES</h1>
+          <li v-for="(item, index) in footerListSites" :key="index">
+            {{ item }}
+          </li>
+        </ul>
+      </div>
+      <div class="logo d-flex">
+        <img src="/img/dc-logo-bg.png" alt="" />
+      </div>
     </div>
   </div>
   <footer class="bottom-footer">
     <div class="container d-flex">
-      <div class="col signUpButton">SIGN-UP NOW!</div>
-      <div class="col-3 d-flex links">
+      <div class="signUp-button">SIGN-UP NOW!</div>
+      <div class="col-5 d-flex links">
         <h2>FOLLOW US</h2>
         <img src="/img/footer-facebook.png" alt="" />
         <img src="/img/footer-twitter.png" alt="" />
@@ -63,12 +68,66 @@ export default {
   background-repeat: no-repeat;
   background-size: cover;
   .container {
-    max-height: 400px;
-    flex-direction: column;
-    flex-wrap: wrap;
-    ul {
-      list-style: none;
-      padding: 0;
+    width: 100%;
+    height: 100%;
+    .links {
+      max-height: 400px;
+      flex-direction: column;
+      flex-wrap: wrap;
+
+      padding: 1rem 0;
+
+      h1 {
+        font-size: 1rem;
+        padding-bottom: 1rem;
+      }
+      ul {
+        list-style: none;
+        padding: 0;
+        li {
+          font-size: 0.8rem;
+          padding-right: 1rem;
+        }
+      }
+    }
+
+    .logo {
+      overflow: hidden;
+      max-height: 350px;
+      align-items: center;
+      justify-content: flex-end;
+
+      img {
+        width: 80%;
+        aspect-ratio: 1;
+      }
+    }
+  }
+}
+
+.bottom-footer {
+  background-color: #303030;
+  color: white;
+  .container {
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem 0;
+    .signUp-button {
+      border: 2px solid blue;
+      padding: 0.5rem;
+    }
+    div.d-flex {
+      justify-content: space-around;
+      align-items: center;
+
+      h2 {
+        font-size: 1.5rem;
+        padding-right: 0.5rem;
+      }
+      img {
+        padding: 0 0.5rem;
+        height: 2rem;
+      }
     }
   }
 }
